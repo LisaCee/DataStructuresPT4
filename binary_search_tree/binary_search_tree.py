f83f35b912dff6b2fa6fa4c9e1509baed91c19ba
LisaCee <lisaceedesign@gmail.com>
2020-01-09 15:45:56 -0800

    added iterative solution for get_max

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -43,15 +43,22 @@ class BinarySearchTree:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if not self:
-            return None
-        if self.right:
-            return self.right.get_max()
-        else:
-            return self.value
+        # if not self:
+        #     return None
+        # if self.right:
+        #     return self.right.get_max()
+        # else:
+        #     return self.value
+
+        # iterative solution
+        curr_tree_root = self
+        while curr_tree_root.right:
+            curr_tree_root = curr_tree_root.right
+        return curr_tree_root.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
+
     def for_each(self, cb):
         if self.right:
             cb(self)
